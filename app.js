ca47051cd79a3654ae95a23461cabb3a05df3cc1
Subhasree370 <subhasreemaji190@gmail.com>
2023-03-19 12:45:46 +0530

    CORS policy update

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/", require("./routes/index"));
 
 app.listen(PORT, async () => {
