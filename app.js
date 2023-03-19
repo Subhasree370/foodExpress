7005c511aad1deb65ddf6e8f2b98376f6e9fe825
Subhasree370 <subhasreemaji190@gmail.com>
2023-03-19 13:06:00 +0530

    CORS policy update

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(fileUpload());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
